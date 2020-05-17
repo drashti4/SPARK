080a7f74ccda0523d17f0e1a342b107a9fbfe951
rajnisohpal <rajnisohpal89@gmail.com>
2020-05-17 13:47:11 -0400

    Updated TrafficIncident component

--- src/components/TrafficIncidents.js
+++ src/components/TrafficIncidents.js
@@ -1,21 +1,16 @@
 import React, { useEffect, useState } from "react";
 import geoPoint from "geopoint";
-import IncidentCategory from "./IncidentCategory";
-import IncidentData from "./IncidentData";
+import publicIP from 'public-ip'
 import IncidentLegend from "./IncidentLegend";
-import IncidentInput from "./IncidentInput";
 import "./trafficincidents.css";
 
 // Your API KEY can be hardcoded, but I recommend setting it as an env variable.
 const API_KEY = "ohVRNxhVQsszHT52o64ZlXKLPu6yxame";
 
 const TrafficIncidents = () => {
-  const [error, seterror] = useState(null);
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(true);
   const [trafficData, setTrafficData] = useState([]);
-  const [latitude, setLatitude] = useState("");
-  const [longitude, setLongitude] = useState("");
 
   let date = new Date();
   let currentDate = date.toDateString();
@@ -30,96 +25,47 @@ const TrafficIncidents = () => {
     return bboxValues;
   };
 
-  const loadData = () => {
-    const response = async () =>
-      getBoundingBox(parseInt(latitude), parseInt(longitude)).then((values) => {
-        fetch(
-          `https://api.tomtom.com/traffic/services/4/incidentDetails/s3/${values[0]._degLat},${values[0]._degLon},${values[1]._degLat},${values[1]._degLon}/10/-1/json?projection=EPSG4326&key=${API_KEY}`
-        )
-          .then((res) => res.json())
-          .then((data) => console.log(data));
-      });
-
-    // fetch(
-    //   `https://api.tomtom.com/traffic/services/4/incidentDetails/s3/${values[0]._degLat},${values[0]._degLon},${values[1]._degLat},${values[1]._degLon}/10/-1/json?projection=EPSG4326&key=${API_KEY}`
-    // )
-    //   .then((res) => res.json())
-    //   .then((data) => console.log(data));
-  };
-
-  if (isSubmitted) {
-    loadData();
-  }
-
-  // componentDidMount() {
-  // publicIP
-  //   .v4()
-  //   .then((ip) => fetch(`https://ipapi.co/${ip}/json`))
-  //   .then((res) => res.json())
-  //   .then((result) => this.getBoundingBox(result.latitude, result.longitude))
-  //   .then((values) =>
-  //     fetch(
-  //       `https://api.tomtom.com/traffic/services/4/incidentDetails/s3/${values[0]._degLat},${values[0]._degLon},${values[1]._degLat},${values[1]._degLon}/10/-1/json?projection=EPSG4326&key=${API_KEY}`
-  //     )
-  //   )
-  //   .then((res) => res.json())
-  //   .then(
-  //     (payload) => {
-  //       this.setState({
-  //         isLoaded: true,
-  //         trafficData: payload["tm"]["poi"],
-  //       });
-  //     },
-  //     (error) => {
-  //       this.setState({
-  //         isLoaded: true,
-  //         error,
-  //       });
-  //     }
-  //   );
-  // }
-
-  const toggleIncidents = (e) => {
-    e.preventDefault();
-    document.querySelector(".incidents-table").classList.toggle("hide");
-  };
+    useEffect(()=> {
+  publicIP
+    .v4()
+    .then((ip) => fetch(`https://ipapi.co/${ip}/json`))
+    .then((res) => res.json())
+    .then((result) => getBoundingBox(result.latitude, result.longitude))
+    .then((values) =>
+      fetch(
+        `https://api.tomtom.com/traffic/services/4/incidentDetails/s3/${values[0]._degLat},${values[0]._degLon},${values[1]._degLat},${values[1]._degLon}/10/-1/json?projection=EPSG4326&key=${API_KEY}`
+      )
+    )
+    .then((res) => res.json())
+    .then(
+      (payload) => {
+        setIsLoaded(true);
+        setTrafficData(payload)
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error,
+        });
+        setIsLoaded(true);
+        setError(error);
+      }
+    );
+  },[]);
 
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
     return <div>Loading...</div>;
-  } else if (!isSubmitted) {
-    return (
-      <IncidentInput
-        setIsSubmitted={setIsSubmitted}
-        isSubmited={isSubmitted}
-        setLatitude={setLatitude}
-        setLongitude={setLongitude}
-        latitude={latitude}
-        longitude={longitude}
-      />
-    );
   } else {
     return (
       <div className="incidents-wrapper">
         <h1>Traffic Incidents</h1>
         <h5>{currentDate}</h5>
         <h5>Time: {currentTime}</h5>
-        <div className="incidents-data">
-          <input
-            type="submit"
-            value="Show Incidents"
-            onClick={toggleIncidents}
-          />
-          <input
-            type="submit"
-            value="Search Again"
-            onClick={() => setIsSubmitted(!isSubmitted)}
-          />
-          <table className="incidents-table hide">
-            <IncidentCategory />
-            <IncidentData data={trafficData} />
-          </table>
+        <div className='json-data'>
+          <h3>JSON DATA</h3>
+        {JSON.stringify(trafficData)}  
         </div>
         <IncidentLegend />
       </div>
