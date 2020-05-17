17ba4a39d57277db0d87898d6ec0f232c1cbb8ab
rajnisohpal <rajnisohpal89@gmail.com>
2020-05-17 05:12:57 -0400

    Added IncidentTraffic components

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+import TrafficIncidents from "./components/TrafficIncidents";
+function App() {
+  return <TrafficIncidents />;
+}
+
+export default App;
